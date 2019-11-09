47aa85ed11646f49f4fdae64271688da50c92629
tsstnt <tsstnt@163.com>
2019-11-09 13:51:39 +0800

    結構完成

--- src/pages/Msite/Msite.vue
+++ src/pages/Msite/Msite.vue
@@ -13,11 +13,11 @@
         </span>
     </header> -->
     <GshopHeader title="昌平区北七家宏福科技园(337省道北)">
-      <span  slot="right" class="header_login">
-        <span class="header_login_text">登录|注册</span>
-      </span>
       <span slot="left" class="header_search">
         <i class="iconfont icon-sousuo"></i>
+      </span>
+      <span  slot="right" class="header_login">
+        <span class="header_login_text">登录|注册</span>
       </span>
     </GshopHeader>
     <!--首页导航-->
